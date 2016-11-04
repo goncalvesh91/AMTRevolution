f8f22b64c73d9f1d10ecb2f2a991b39ad5bd6eaf
goncalvesh91 <hfdsgoncalves@gmail.com>
2016-11-04 21:11:05 +0000

    v001
    [+] Entry point before main window
    [+] Correct .NET Framework
    [+] .gitignore update

--- AMTRevolution/GUI/MainWindow.xaml.cs
+++ AMTRevolution/GUI/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// AMTRevolution
+// Hugo Gonçalves
+// Rui Gonçalves
+
+using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
---
+++ AMTRevolution/main.cs
@@ -0,0 +1,21 @@
+﻿// AMTRevolution
+// Hugo Gonçalves
+// Rui Gonçalves
+
+using System.Windows;
+
+namespace AMTRevolution
+{
+    public partial class App : Application
+    {
+        void AMTRevolution_Startup(object sender, StartupEventArgs e)
+        {
+            // Initial AMTRevolution Checks
+
+
+            // Run the main window
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
