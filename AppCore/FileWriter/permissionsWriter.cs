4656313e2b4fcfe2c321f49d6d5e307a88538d2e
goncalvesh91 <hfdsgoncalves@gmail.com>
2016-11-07 16:55:24 +0000

    v001
    [+] User permissions class

--- AppCore/FileWriter/permissionsWriter.cs
+++ AppCore/FileWriter/permissionsWriter.cs
@@ -3,6 +3,10 @@
 // Rui Gonçalves
 
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace AppCore.FileWriter
 {
@@ -10,6 +14,63 @@ namespace AppCore.FileWriter
 	{
 		public permissionsWriter()
 		{
+			// Check if permissions file exists, if not create it
+			if(!File.Exists(AppSettings.AppSettings.permissionsFilePath))
+			{
+				generatePermissionsFile(AppSettings.AppSettings.permissionsFilePath);
+			}
+		}
+		
+		private bool generatePermissionsFile(string path)
+		{
+			try
+			{
+				// Generate file with the root users
+				var permFile = new List<userPermission>();
+				var userPerm = new userPermission("gonalvhf","Hugo Gonçalves",1);
+				permFile.Add(userPerm);
+				userPerm = new userPermission("goncarj3","Rui Gonçalves",1);
+				permFile.Add(userPerm);
+				userPerm = new userPermission("Caramelos","Rui Gonçalves",1);
+				permFile.Add(userPerm);
+				userPerm = new userPermission("Hugo Gonçalves","Hugo Gonçalves",1);
+				permFile.Add(userPerm);
+				var file = File.OpenWrite(path);
+				var writer = new BinaryFormatter();
+				writer.Serialize(file, permFile);
+				file.Close();
+				return true;
+			}
+			catch(Exception ex)
+			{
+				var eventHandler = new AppEvent.appEvent(AppSettings.AppSettings.appEventsPath);
+				eventHandler.addAppEvent(DateTime.Now,"Failed to generate permissions files");			
+				return false;
+			}
+		}
+		
+		public int getUserPermission(string user)
+		{
+			
+			return 0;
+		}
+		
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+		[Serializable]
+		public struct userPermission
+		{
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 50)]
+			public string userName;
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 50)]
+			public string name;
+			public int permission;
+			
+			public userPermission(string _userName, string _name, int _permission)
+			{
+				this.userName = _userName;
+				this.name = _name;
+				this.permission = _permission;
+			}
 		}
 	}
 }
